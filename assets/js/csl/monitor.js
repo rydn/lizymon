73802e69ddad888ed8ea0caa37fe3860c0e60c39
Ryan Dick <noxinoobin@gmail.com>
2012-08-24 02:42:28 +0200

    added latency rendering

--- assets/js/csl/monitor.js
+++ assets/js/csl/monitor.js
@@ -1,26 +1,38 @@
 var socket = io.connect('http://localhost');
 
-//	CPU USAGE
-socket.on('mon_cpu', function(cpu) {
-	var latency = Date() - cpu.timestamp;
-	$('.cpu').html('<li>' + JSON.stringify(cpu) + '</li>');
-});
 //	PROCCESORS
+socket.on('mon_cpu', function(cpus) {
+	var date = new Date();
+	var currrentLatency = date.getTime() - cpus.timestamp;
+	page.latency.update(currrentLatency);
+	//$('.cpu').html('<li>' + JSON.stringify(cpu) + '</li>');
+});
+
+//	CPU USAGE
 socket.on('mon_proc', function(proc) {
-	var latency = Date() - proc.timestamp;
-	$('.proc').html('<li>' + JSON.stringify(proc) + '</li>');
-	$('.gauge_proc_percentage').width(proc.cpuPercent +'%');
-	$('.gauge_proc_percentage').text(proc.cpuPercent +'% cpu usage');
+	
+	//$('.proc').html('<li>' + JSON.stringify(proc) + '</li>');
+	$('.gauge_proc_percentage').width(proc.cpuPercent + '%');
+	$('.gauge_proc_percentage').text(proc.cpuPercent + '% cpu usage');
+
+	var date = new Date();
+	var currrentLatency = date.getTime() - proc.timestamp;
+	page.latency.update(currrentLatency);
 });
 //	MEMORY
 socket.on('mon_mem', function(mem) {
-	page.latency.current = Date() - mem.timestamp;
-	$('.mem').html('<li>' + JSON.stringify(mem) + '</li>');
+	
+	//$('.mem').html('<li>' + JSON.stringify(mem) + '</li>');
 
-	var usedPercent = (Math.floor((mem.free /mem.total)*1000)/10);
+	var usedPercent = (Math.floor((mem.free / mem.total) * 1000) / 10);
 	$('.gauge_memory_free').width(usedPercent + '%');
 	$('.gauge_memory_free').text(usedPercent + '% free');
 
 	$('.gauge_memory_used').width((100 - usedPercent) + '%');
 	$('.gauge_memory_used').text((100 - usedPercent) + '% used');
+
+	var date = new Date();
+	var currrentLatency = date.getTime() - mem.timestamp;
+	page.latency.update(currrentLatency);
 });
+
